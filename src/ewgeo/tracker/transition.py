Get chapter 9 figures running properly with better gates and process covariance. Update initiators and motion models to fix inconsistency in CJ derivation, update tests accordingly.

src/ewgeo/tracker/transition.py
@@ -254,7 +254,7 @@ class ConstantVelocityMotionModel(MotionModel):
         Implement the process noise covariance Q for a constant velocity motion model.
 
         Process noise is an acceleration disturbance.
-        :param process_covar: Variance term, in units of m^2/s^3
+        :param process_covar: Variance term, in units of m^2/s^4
         """
         process_covar = self.validate_process_covar_input(process_covar)
         if time_delta is None:
@@ -355,22 +355,22 @@ class ConstantJerkMotionModel(MotionModel):
         if time_delta is None:
             time_delta = self.time_delta
 
-        q_mat = np.block([[time_delta ** 7 / 252 * process_covar,
-                           time_delta ** 6 / 72 * process_covar,
-                           time_delta ** 5 / 30 * process_covar,
-                           time_delta ** 4 / 24 * process_covar],
-                          [time_delta ** 6 / 72 * process_covar,
-                           time_delta ** 5 / 20 * process_covar,
-                           time_delta ** 4 / 8 * process_covar,
-                           time_delta ** 3 / 6 * process_covar],
-                          [time_delta ** 5 / 30 * process_covar,
-                           time_delta ** 4 / 8 * process_covar,
-                           time_delta ** 3 / 3 * process_covar,
-                           time_delta ** 2 / 2 * process_covar],
-                          [time_delta ** 4 / 24 * process_covar,
-                           time_delta ** 3 / 6 * process_covar,
-                           time_delta ** 2 / 2 * process_covar,
-                           time_delta * process_covar]])
+        q_mat = np.block([[time_delta ** 6 / 36 * process_covar,
+                           time_delta ** 5 / 12 * process_covar,
+                           time_delta ** 4 / 6  * process_covar,
+                           time_delta ** 3 / 6  * process_covar],
+                          [time_delta ** 5 / 12 * process_covar,
+                           time_delta ** 4 / 4  * process_covar,
+                           time_delta ** 3 / 2  * process_covar,
+                           time_delta ** 2 / 2  * process_covar],
+                          [time_delta ** 4 / 6  * process_covar,
+                           time_delta ** 3 / 2  * process_covar,
+                           time_delta ** 2       * process_covar,
+                           time_delta            * process_covar],
+                          [time_delta ** 3 / 6  * process_covar,
+                           time_delta ** 2 / 2  * process_covar,
+                           time_delta            * process_covar,
+                           process_covar]])
         return CovarianceMatrix(q_mat)
 
 

tests/test_tracker_transition.py
@@ -122,10 +122,10 @@ def test_cj_transition_matrix_1d_dt1():
 def test_cj_process_noise_1d_dt1():
     model = ConstantJerkMotionModel(num_dims=1, process_covar=1.0)
     Q = model.make_process_covariance_matrix(process_covar=1.0, time_delta=1.0)
-    expected = np.array([[1/252, 1/72, 1/30, 1/24],
-                         [1/72,  1/20, 1/8,  1/6 ],
-                         [1/30,  1/8,  1/3,  1/2 ],
-                         [1/24,  1/6,  1/2,  1.0 ]])
+    expected = np.array([[1/36,  1/12, 1/6,  1/6 ],
+                         [1/12,  1/4,  1/2,  1/2 ],
+                         [1/6,   1/2,  1.0,  1.0 ],
+                         [1/6,   1/2,  1.0,  1.0 ]])
     assert equal_to_tolerance(Q.cov, expected)
 
 